084a35f68b748230284320817a13fe5022f417a5
Dan Raskin <Danieltraskin@gmail.com>
2023-09-13 12:45:01 -0500

    script functions declared separately from while loop

--- timecard.zsh
+++ timecard.zsh
@@ -1,14 +1,9 @@
 #!/usr/bin/env zsh
 
-declare -A project_start_times
-declare -A project_total_times
+declare -A project_times
+# declare -A project_total_times
 CSV_FILE="timetracker.csv"
 highest_index=0
-
-# generate unicode
-# function generate_unicode_char {
-#   echo -e "\U$((0x1F600 + RANDOM % 50))"
-# }
 
 # Function to read data from CSV file
 function read_csv_data {
@@ -31,38 +26,35 @@ function set_prompt {
 # Function to write data to the CSV file
 function write_csv_data {
   index=$((highest_index + 1))
-  # echo "index,project,start_time,stop_time" > "$CSV_FILE"
   echo "$index,$project,$start_time,$stop_time" >> "$CSV_FILE"
   (( index++ ))
-  # for project in ${(k)project_start_times}; do
-  #   for start_time in ${(P)project_start_times[$project]}; do
-  #     stop_time=${project_total_times["$index:$project:$start_time"]:-}
-  #     echo "$index,$project,$start_time,$stop_time" >> "$CSV_FILE"
-  #     (( index++ ))
-  #   done
-  # done
 }
 
 # Load existing data from CSV file, if it exists
 IFS=,
 while read -r index project start_time stop_time; do
-  project_start_times["$index:$project"]=$start_time
-  project_total_times["$index:$project"]=$stop_time
-  if  [[ index =~ ^[0-9]+$ && (( index > highest_index )) ]] ; then
+  project_times["$index:$project"]=$start_time
+  # project_total_times["$index:$project"]=$stop_time
+  if  [[ (( index > highest_index )) ]] ; then
     highest_index=$index
-    echo $index $highest_index
   fi
 done < <(read_csv_data)
 unset IFS
 
+function cmd_print {
+  echo "Total time spent on $project: seconds."
+}
+
+function cmd_quit {
+  echo "Exiting..."
+  exit 0
+}
+
+
+
 
 # Initialize the program
 echo "Time Tracker"
-
-# Hook the function to the 'precmd' hook, which runs before each prompt display
-# autoload -U add-zsh-hook
-# add-zsh-hook precmd set_prompt
-
 
 # Process user commands
 while true; do
@@ -71,8 +63,7 @@ while true; do
   read command
 
   if [[ "$command" == "quit" ]]; then
-    echo "Exiting..."
-    exit 0
+    cmd_quit
   elif [[ "$command" =~ ^-p ]]; then
     project=${command#-p }  # Extract project name from input
     echo "To begin work on $project, enter 'start'"
@@ -81,11 +72,11 @@ while true; do
 
     if [[ "$start_input" == "start" ]]; then
       start_time=$(date -u +%s)
-      project_start_times["$project"]=$start_time
-      # echo "Started tracking work on $project. start time is $start_time. project start times array is ${project_start_times["$project"]}"
+      project_times["$project"]=$start_time
+      # echo "Started tracking work on $project. start time is $start_time. project start times array is ${project_times["$project"]}"
     fi
   elif [[ "$command" == "print" ]]; then
-    print_project_times
+    cmd_print
   else
     echo "Invalid input."
     set_prompt
@@ -97,7 +88,7 @@ while true; do
   read stop_input
 
   if [[ "$stop_input" == "stop" ]]; then
-    if [[ -n ${project_start_times["$project"]} ]]; then
+    if [[ -n ${project_times["$project"]} ]]; then
       stop_time=$(date -u +%s)
       echo "Stopped tracking work on $project."
       write_csv_data
